30ca54d86cd502edc4f41c954fa09005a59f1d24
zoozero <zzzkimjy@korea.ac.kr>
2021-05-07 14:29:49 +0900

    feat: 로그인 기능에서 하이 추가

--- app/src/main/java/com/example/runningappvita/LoginActivity.kt
+++ app/src/main/java/com/example/runningappvita/LoginActivity.kt
@@ -3,9 +3,11 @@ package com.example.runningappvita
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+        println("hi")
+
     }
 }
